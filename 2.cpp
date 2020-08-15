0a271e36415feb12ba117e138aaf9b30c2c16785
Naman Gupta <67692721+naman153@users.noreply.github.com>
2020-08-15 11:33:07 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+// Complete the dayOfProgrammer function below.
+string dayOfProgrammer(int year) {
+   short int feb; //days in february
+    int mon7=31+31+30+31+30+31+31;
+    if(year < 1918) //julian
+        feb = year%4 ? 28 : 29;
+    else if(year > 1918) //gregorian
+        feb = !(year%400) || year%100 && !(year%4) ? 29 : 28;
+    else //dreaded 1918
+        feb = 15;
+    feb = 256 - (feb + mon7);
+    return to_string(feb) + ".09." + to_string(year);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string year_temp;
+    getline(cin, year_temp);
+
+    int year = stoi(ltrim(rtrim(year_temp)));
+
+    string result = dayOfProgrammer(year);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
